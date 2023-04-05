a52577be7fb5b10cc528a9673df3abb087ff43ec
johns <johns@Johnsy>
2023-04-05 15:43:41 +0530

    After change

--- src/test/java/Practice/Test3.java
+++ src/test/java/Practice/Test3.java
@@ -5,14 +5,14 @@ import org.testng.annotations.Test;
 public class Test3 {
 @Test	
 	public void Method1() {
-		System.out.println("----Test01------");
+		System.out.println("----Test010------");
 	}
  @Test
 	public void Method2() {
-	System.out.println("----Test02------");	
+	System.out.println("----Test020------");	
 	}
  @Test
         public void Method3() {
-		System.out.println("----Test03------");	
+		System.out.println("----Test030------");	
 		}
 }
